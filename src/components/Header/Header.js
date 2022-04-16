dc384845b8fe4d3dfee2b75ba6d027dd7fc1f7ee
rahadislam <rahadislm.web@gmil.com>
2022-04-17 02:26:52 +0600

    header area complete design

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css'
+import logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <Navbar  expand="lg">
+            <Container>
+                <img width="130"  src={logo} alt="" />
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                    </Nav>
+                    <Nav className="d-flex customHeader">
+                        <Link to='/home'>Home</Link>
+                        <Link to='/about'>About</Link>
+                        <Link to='/blog'>Blog</Link>
+                        <Link to='/home'>Cheak Out</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
